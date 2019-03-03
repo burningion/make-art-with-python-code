8af207ba1bed0f5690e39c8d403855f8b24b724a
Kirk Kaiser <kirk@zothcorp.com>
2019-03-03 15:18:10 -0500

    Add Chapter 3

---
+++ chapter3/README.md
@@ -0,0 +1,13 @@
+# Chapter 3 - Writing Our First Graphics Program
+
+In this chapter, we install the `pygame` library, and use it to draw our very first pixel on the screen.
+
+From here, we use the `for` loop to make our very first line cross the screen.
+
+We then see how to make our lines diagonal.
+
+## Challenges
+
+Try drawing a square with multiple lines across the screen.
+
+Try drawing lines that form a triangle.
---
+++ chapter3/straightLine.py
@@ -0,0 +1,29 @@
+import pygame
+import pygame.gfxdraw
+
+pygame.init()
+
+screenWidth = 800
+screenHeight = 800
+
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+running = True
+
+while running:
+    # fill the screen with black
+    screen.fill(black)
+
+    # Our for loop, for the width of the screen
+    for i in range(0, screenWidth):
+        # Our pixel draw function uses i to know the current value
+        pygame.gfxdraw.pixel(screen, i, screenHeight // 2, white)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
